7e61549ae1f53557b147bc53c11e0d5a79ac4b10
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-07 16:08:30 -0700

    switching from Jupyter slides to RStudio Reveal.js

--- render-slides.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-#
-# Copyright (C) 2016 M. Edward (Ed) Borasky <znmeb@znmeb.net>
-# License: MIT
-
-source ~/jupyter/bin/activate
-source ~/.profile
-jupyter nbconvert reveal.ipynb --to slides --post serve
