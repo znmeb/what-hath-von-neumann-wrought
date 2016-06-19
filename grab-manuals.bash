d6a58ac022b0fb1d5dc0f690cd022038dcda39ff
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-19 16:11:36 -0700

    ditch the Bitsavers download

--- grab-manuals.bash
+++ grab-manuals.bash
@@ -1,9 +1,5 @@
 #! /bin/bash
 
-# Bitsavers
-wget --mirror --convert-links --backup-converted --html-extension \
-  http://bitsavers.trailing-edge.com/pdf/univOfIllinoisUrbana/illiac/ILLIAC/
-
 # Nash from Archive.org
 aria2c https://archive.org/download/illiacprogrammin00univ/illiacprogrammin00univ_archive.torrent
 
