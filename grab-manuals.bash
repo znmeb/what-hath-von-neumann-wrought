f700bd8d962f5715c5180b6a24c4c5bc0a57c1f6
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2016-06-22 18:47:21 -0700

    cleanup

--- grab-manuals.bash
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Nash from Archive.org
-aria2c https://archive.org/download/illiacprogrammin00univ/illiacprogrammin00univ_archive.torrent
-
-# Fosdick from Archive.org
-aria2c https://archive.org/download/guidetoilliacpro00fosd/guidetoilliacpro00fosd_archive.torrent
